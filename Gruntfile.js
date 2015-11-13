88c49c76d50d60506e6298dd59809bf39c84cd50
zhy123 <cs09bzhang@163.com>
2015-11-13 16:38:04 +0800

    start grunt

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				stripBanners: true,
+				banner: '/* <%-pkg.name%>-<%pkg.version%>.js <%- grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			build: {
+				src: 'src/test.js',
+				dest: 'build/<%-pkg.name%>-<%pkg.version%>.js.min.js'
+			} 
+		},
+		jshint:{
+			//test1: ['Gruntfile.js'],
+			//test2: ['src/*.js'],
+			build: ['Gruntfile.js', 'src/*.js'],
+			options: {
+				//jshintrc: '.jshintrc'
+				globals: {
+					jQuery: true
+				}
+			}
+		},
+		watch: {
+			bulid: {
+				files: ['src/*.js', 'src/*.css'],
+				tasks: ['jshint', 'uglify'],
+				options: { spawn: false}
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default', ['jshint','uglify','watch']);
+};
